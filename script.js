8312c5f6cc07b0d3c5ecde00f66dd91cafee87c2
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-02-03 12:54:32 +0530

    Data from local storage is populated on UI for selected seats, selected movie, and corresponding count and price text. This persists even after page reload.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)'); // grabs t
 const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
+
 let ticketPrice = +movieSelect.value; // "+" is same as using parseInt(). And 'let' is used as it will be changing w.r.t the movie selected.
 
 // FUNCTIONS
@@ -33,8 +34,39 @@ const updateSelectedCount = () => {
 const setMovieData = (movieIndex, moviePrice) => {
   localStorage.setItem('selectedMovieIndex', movieIndex);
   localStorage.setItem('selectedMoviePrice', moviePrice);
-  // So, now if a movie and seats for it are selected, then the data for 'selectedSeats', 'selectedMovieIndex', and 'selectedMoviePrice' are stored in local storage. On reload, the data persists in the browser. Next, it must be reflected in the UI.
+  // So, now if a movie and seats for it are selected, then the data for 'selectedSeats', 'selectedMovieIndex', and 'selectedMoviePrice' are stored in local storage. On reload, the data persists in the browser. Using 'populateUI' function, reflected in the UI.
 };
+
+// Get data from localstorage and populate UI
+const populateUI = () => {
+  // Function to populate the data in local storage on UI (selected seats and price)
+  // Getting selectedSeats from LS
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  // While adding to LS, JSON.stringify was used to convert array to string. Now, using JSON.parse, the opposite done. That is, string is parsed to an array.
+
+  // console.log(selectedSeats); // logs the array of seats selected
+
+  // First, make sure that a seat is selected:
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      // the value of 'indexOf()' will be -1 if the elements is not in the array. So, check that seat (with specifc index) is selected, and class of 'selected' is added to it:
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  // The above reflects the selected seats on UI. Now, the price text must be updated too:
+  // Get the selectedMovieIndex data from LS:
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    // To display the selected movie from LS and stay even after reload:
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+};
+
+populateUI();
 
 // EVENT LISTENERS
 
@@ -62,3 +94,6 @@ movieSelect.addEventListener('change', (e) => {
   setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
+
+// Initial count and totol set to show always:
+updateSelectedCount();
