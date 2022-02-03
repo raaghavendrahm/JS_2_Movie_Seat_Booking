3b2a612060df77bec23e4ab4f9fc95fb402140a3
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-02-03 12:15:26 +0530

    setMovieData function is created to store the data of the movie selected and the seat selected.So, now if a movie and seats for it are selected, then the data for "selectedSeats", "selectedMovieIndex", and "selectedMoviePrice" are stored in local storage. On reload, the data persists in the browser. Next, it must be reflected in the UI.

--- script.js
+++ script.js
@@ -14,11 +14,26 @@ const updateSelectedCount = () => {
 
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+  // 'selectedSeats' will be a nodeList of selected elements (seats). It must be converted to a regular array to form an index of it. This can be done with spread operator:
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  // console.log(seatsIndex);
+
+  // Local Storage:
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex)); // This stores selected seats on local storage (but not on UI yet).
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
 
   total.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// Save selected movie index and price:
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+  // So, now if a movie and seats for it are selected, then the data for 'selectedSeats', 'selectedMovieIndex', and 'selectedMoviePrice' are stored in local storage. On reload, the data persists in the browser. Next, it must be reflected in the UI.
 };
 
 // EVENT LISTENERS
@@ -42,5 +57,8 @@ container.addEventListener('click', (e) => {
 movieSelect.addEventListener('change', (e) => {
   // To update the ticket price when a different movie is selected:
   ticketPrice = +e.target.value;
+  // console.log(e.target.selectedIndex, e.target.value);
+  // In the above, 'selectedIndex' is the index of the movie selected. If 'Joker' is selected then 'selectedIndex' is '1' and if 'The Lion King' is selected, then 'selectedIndex' is '3'. And, 'value' is the ticket price. So, if 'The Lion King' is selected, then the 'selectedIndex' is '3', and the 'value' is '12'. So, these two shall be saved to local storage using 'setMovieData' function:
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
